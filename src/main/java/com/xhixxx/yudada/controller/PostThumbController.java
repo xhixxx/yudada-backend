7be6b63e23598d8600eb93fe0508f9576b50d738
xhixxx <loongcyan@yeah.net>
2024-09-29 21:53:24 +0800

    init: backend-init and database-init

---
+++ src/main/java/com/xhixxx/yudada/controller/PostThumbController.java
@@ -0,0 +1,54 @@
+package com.xhixxx.yudada.controller;
+
+import com.xhixxx.yudada.common.BaseResponse;
+import com.xhixxx.yudada.common.ErrorCode;
+import com.xhixxx.yudada.common.ResultUtils;
+import com.xhixxx.yudada.exception.BusinessException;
+import com.xhixxx.yudada.model.dto.postthumb.PostThumbAddRequest;
+import com.xhixxx.yudada.model.entity.User;
+import com.xhixxx.yudada.service.PostThumbService;
+import com.xhixxx.yudada.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 帖子点赞接口
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+                                         HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
---
+++ src/main/java/com/xhixxx/yudada/mapper/PostThumbMapper.java
@@ -0,0 +1,15 @@
+package com.xhixxx.yudada.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xhixxx.yudada.model.entity.PostThumb;
+
+/**
+ * 帖子点赞数据库操作
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
